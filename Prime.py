3611836d2fa99fb670a4e9b54f22320a1e27d33c
Ashish Kumar Panda <44130583+Ashindustry007@users.noreply.github.com>
2020-09-22 15:28:55 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -20,5 +20,7 @@ def isPrime(n) :
     return True
     
     '''
-    All primes are of the form 6k ± 1, with the exception of 2 and 3. This is because all integers can be expressed as (6k + i) for some integer k and for i = ?1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3). So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1.
+    All primes are of the form 6k ± 1, with the exception of 2 and 3.
+    This is because all integers can be expressed as (6k + i) for some integer k and for i = ?1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3).
+    So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1.
     '''
